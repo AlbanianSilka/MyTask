59c5e2d072a8db533efd55195eab09d3373c5ed8
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-08-17 15:57:48 +0300

    Created first routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts do
+    get :swap_words
+    # collection do
+    #   patch :swap_words
+    # end
+  end
+
+  resources :keywords
+  root "posts#index"
 end
