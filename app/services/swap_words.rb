571da175e74a20b080da5a6cf9bafc2ec17d219f
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-08-20 18:55:36 +0300

    Added swap limits to service

--- app/services/swap_words.rb
+++ app/services/swap_words.rb
@@ -2,17 +2,25 @@
 
 # Service for swap words def
 class SwapWords
+  include ActionController::Helpers
+  include ActionController::Flash
+
   def initialize(post)
     @post = post
   end
 
   def execute
-    all_keys = Keyword.all.pluck(:key, :link).to_h.transform_keys(&:downcase)
-    @post.swap_content = @post.swap_content.to_s.gsub!(/\w+/) do |word|
-      url = all_keys[word.downcase]
-      url ? "<a href='#{url}'>#{word}</a>" : word
+    if @post.swap_limit_counter < @post.swap_limit
+      all_keys = Keyword.all.pluck(:key, :link).to_h.transform_keys(&:downcase)
+      @post.swap_content = @post.swap_content.to_s.gsub!(/\w+/) do |word|
+        url = all_keys[word.downcase]
+        url ? "<a href='#{url}'>#{word}</a>" : word
+      end
+      @post.content = @post.swap_content.to_plain_text.delete!("\n").strip
+      @post.swap_limit_counter = @post.swap_limit_counter + 1
+      @post.save!
+    else
+      nil
     end
-    @post.content = @post.swap_content.to_plain_text.delete!("\n").strip
-    @post.save!
   end
 end
