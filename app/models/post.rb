cb611f09a810e6a88214c9efacb57448560f0eb6
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-08-30 11:53:29 +0300

    Added swap_limit validation to the model and deleted 'swap_words' attribute.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@
 
 # class to describe the actual posts model
 class Post < ApplicationRecord
-  has_rich_text :swap_content
+  validate :non_zero
+  has_rich_text :content
 
+  def non_zero
+    errors.add(:swap_limit, 'Swap limit cannot be negative or equal to zero') if swap_limit <= 0
+  end
 end
