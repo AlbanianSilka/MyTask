d81e93954fa1a59bb91d68e7daf694dcd53afa22
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-08-31 18:50:04 +0300

    Added invalid_keyword to keyword factory.

--- spec/factories/keyword.rb
+++ spec/factories/keyword.rb
@@ -5,4 +5,9 @@ FactoryBot.define do
     key { 'MyText' }
     link { 'https://www.google.com' }
   end
+
+  factory :invalid_keyword, class: 'Keyword' do
+    key { nil }
+    link { nil }
+  end
 end
